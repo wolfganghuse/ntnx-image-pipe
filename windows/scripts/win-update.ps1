a4eda824bcbe36864780582a800e5b49b3275a29
wolfganghuse <wolfgang.huse@nutanix.com>
2021-12-02 09:05:45 +0100

    Update win-update.ps1

--- windows/scripts/win-update.ps1
+++ windows/scripts/win-update.ps1
@@ -4,6 +4,8 @@
 $ProgressPreference = "SilentlyContinue"
 
 Write-Output "Starting PSWindowsUpdate Installation"
+Write-Output "Starting PSWindowsUpdate Installation"
+
 ## ACTUAL NOT WORKING SCRIPT
 # # Install PSWindowsUpdate for scriptable Windows Updates
 # $webDeployURL = "https://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc/file/66095/1/PSWindowsUpdate_1.4.5.zip"
